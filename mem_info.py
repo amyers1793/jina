4c3711b4871b971dafdd7748c389cad46089959d
Maximilian Werk <maximilian.werk@jina.ai>
2021-02-24 10:53:11 +0100

    test: added cpu info

--- mem_info.py
+++ mem_info.py
@@ -4,4 +4,5 @@ import time
 
 for i in range(200):
     print(subprocess.run(['vmstat']))
+    print(subprocess.run(['ps -eo pcpu,pid,user,args | sort -k 1 -r | head -10'], shell=True, check=True))
     time.sleep(2)
